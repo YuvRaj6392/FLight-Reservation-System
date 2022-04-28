430ccf8d64a748e8a17ca2bce5e694f4226abe23
Yuv Raj <yuvraj.srivastava6392@gmail.com>
2022-04-28 11:11:28 +0530

    Completed checkpoint 4

--- src/com/company/RegularTicket.java
+++ src/com/company/RegularTicket.java
@@ -2,111 +2,16 @@ package com.company;
 
 import java.time.LocalDateTime;
 
-public class RegularTicket {
-   private String pnr,from,to,departureDateTime,arrivalDateTime,seatNo,specialServices;
-   private float price;
-   private boolean cancelled;
-   private Flight flight;
-   private Passenger passenger;
+public class RegularTicket extends Ticket {
+  private String specialServices;
 
-    public String getPnr() {
-        return pnr;
+  public RegularTicket(String pnr, String from, String to, String departureDateTime, String arrivalDateTime, String seatNo, String specialServices, float price, boolean cancelled, Flight flight, Passenger passenger) {
+     super(pnr,from,to,departureDateTime,arrivalDateTime,seatNo,price,cancelled,flight,passenger);
+        this.specialServices=specialServices;
     }
-
-    public void setPnr(String pnr) {
-        this.pnr = pnr;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
-    public String getTo() {
-        return to;
-    }
-
-    public void setTo(String to) {
-        this.to = to;
-    }
-
-    public String getDepartureDateTime() {
-        return departureDateTime;
-    }
-
-    public void setDepartureDateTime(String departureDateTime) {
-        this.departureDateTime = departureDateTime;
-    }
-
-    public String getArrivalDateTime() {
-        return arrivalDateTime;
-    }
-
-    public void setArrivalDateTime(String arrivalDateTime) {
-        this.arrivalDateTime = arrivalDateTime;
-    }
-
-    public String getSeatNo() {
-        return seatNo;
-    }
-
-    public void setSeatNo(String seatNo) {
-        this.seatNo = seatNo;
-    }
-
     public void setSpecialServices(String specialServices) {
         this.specialServices = specialServices;
     }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    public boolean isCancelled() {
-        return cancelled;
-    }
-
-    public void setCancelled(boolean cancelled) {
-        this.cancelled = cancelled;
-    }
-
-    public Flight getFlight() {
-        return flight;
-    }
-
-    public void setFlight(Flight flight) {
-        this.flight = flight;
-    }
-
-    public Passenger getPassenger() {
-        return passenger;
-    }
-
-    public void setPassenger(Passenger passenger) {
-        this.passenger = passenger;
-    }
-
-    public RegularTicket(String pnr, String from, String to, String departureDateTime, String arrivalDateTime, String seatNo, String specialServices, float price, boolean cancelled, Flight flight, Passenger passenger) {
-        this.pnr = pnr;
-        this.from = from;
-        this.to = to;
-        this.departureDateTime = departureDateTime;
-        this.arrivalDateTime = arrivalDateTime;
-        this.seatNo = seatNo;
-        this.specialServices = specialServices;
-        this.price = price;
-        this.cancelled = cancelled;
-        this.flight=flight;
-        this.passenger=passenger;
-    }
-
     public String getSpecialServices() {
         return "Special Service: "+specialServices;
     }
@@ -114,26 +19,6 @@ public class RegularTicket {
     {
     this.specialServices=specialServices;
     }
-    public String checkStatus()
-    {
-        if(cancelled)
-        {
-            return "cancelled";
-        }
-        else
-        {
-            return "Confirmed";
-        }
-    }
-    public int getFlightDuration()
-    {
-        LocalDateTime departureLocalDateTime = LocalDateTime.parse(departureDateTime);
-        LocalDateTime arrivalLocalDateTime = LocalDateTime.parse(arrivalDateTime);
-        return (arrivalLocalDateTime.getDayOfMonth() - departureLocalDateTime.getDayOfMonth()) * 24 +
-                (arrivalLocalDateTime.getHour() - departureLocalDateTime.getHour());
-    }
-    public void cancel()
-    {
-        cancelled=true;
-    }
+
+
 }
--- src/com/company/TouristTicket.java
+++ src/com/company/TouristTicket.java
@@ -2,149 +2,21 @@ package com.company;
 
 import java.time.LocalDateTime;
 
-public class TouristTicket{
-  private   String pnr,from,to,departureDateTime,arrivalDateTime,seatNo,specialServices,hotelAddress;
+public class TouristTicket extends Ticket{
+    private String hotelAddress;
   private  String[] selectedTouristLocation=new String[5];
-  private float price;
-  private  boolean cancelled;
-  private  Flight flight;
-  private  Passenger passenger;
+  public TouristTicket(String pnr, String from, String to, String departureDateTime, String arrivalDateTime, String seatNo, String specialServices, float price, boolean cancelled, String hotelAddress, Flight flight, Passenger passenger) {
+      super(pnr,from,to,departureDateTime,arrivalDateTime,seatNo,price,cancelled,flight,passenger);
+      this.hotelAddress=hotelAddress;
 
-    public String getPnr() {
-        return pnr;
-    }
-
-    public void setPnr(String pnr) {
-        this.pnr = pnr;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
-    public String getTo() {
-        return to;
-    }
-
-    public void setTo(String to) {
-        this.to = to;
-    }
-
-    public String getDepartureDateTime() {
-        return departureDateTime;
-    }
-
-    public void setDepartureDateTime(String departureDateTime) {
-        this.departureDateTime = departureDateTime;
-    }
-
-    public String getArrivalDateTime() {
-        return arrivalDateTime;
-    }
-
-    public void setArrivalDateTime(String arrivalDateTime) {
-        this.arrivalDateTime = arrivalDateTime;
-    }
-
-    public String getSeatNo() {
-        return seatNo;
-    }
-
-    public void setSeatNo(String seatNo) {
-        this.seatNo = seatNo;
-    }
-
-    public void setSpecialServices(String specialServices) {
-        this.specialServices = specialServices;
     }
 
     public void setHotelAddress(String hotelAddress) {
         this.hotelAddress = hotelAddress;
     }
 
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    public boolean isCancelled() {
-        return cancelled;
-    }
-
-    public void setCancelled(boolean cancelled) {
-        this.cancelled = cancelled;
-    }
-
-    public Flight getFlight() {
-        return flight;
-    }
-
-    public void setFlight(Flight flight) {
-        this.flight = flight;
-    }
-
-    public Passenger getPassenger() {
-        return passenger;
-    }
-
-    public void setPassenger(Passenger passenger) {
-        this.passenger = passenger;
-    }
-
-    public TouristTicket(String pnr, String from, String to, String departureDateTime, String arrivalDateTime, String seatNo, String specialServices, float price, boolean cancelled, String hotelAddress, Flight flight, Passenger passenger) {
-        this.pnr = pnr;
-        this.from = from;
-        this.to = to;
-        this.departureDateTime = departureDateTime;
-        this.arrivalDateTime = arrivalDateTime;
-        this.seatNo = seatNo;
-        this.specialServices = specialServices;
-        this.price = price;
-        this.cancelled = cancelled;
-        this.hotelAddress=hotelAddress;
-        this.flight=flight;
-        this.passenger=passenger;
-    }
-
     public void setSelectedTouristLocation(String[] selectedTouristLocation) {
         this.selectedTouristLocation = selectedTouristLocation;
-    }
-
-    public String getSpecialServices() {
-        return "Special Service: "+specialServices;
-    }
-    public void updateSpecialServices(String specialServices)
-    {
-        this.specialServices=specialServices;
-    }
-    public String checkStatus()
-    {
-       if(cancelled)
-       {
-           return "cancelled";
-       }
-       else
-       {
-           return "Confirmed";
-       }
-    }
-    public int getFlightDuration()
-    {
-        LocalDateTime departureLocalDateTime = LocalDateTime.parse(departureDateTime);
-        LocalDateTime arrivalLocalDateTime = LocalDateTime.parse(arrivalDateTime);
-        return (arrivalLocalDateTime.getDayOfMonth() - departureLocalDateTime.getDayOfMonth()) * 24 +
-                (arrivalLocalDateTime.getHour() - departureLocalDateTime.getHour());
-    }
-    public void cancel()
-    {
-        cancelled=true;
     }
 
     public String getHotelAddress() {
